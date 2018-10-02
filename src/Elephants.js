940f44cd43b6d7f0b1668e04e12f2109d1acc387
Tyler Collier <tyler.collier+github@devmounta.in>
2018-10-02 10:26:51 -0700

    PropTypes

---
+++ src/Elephants.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Elephant extends Component {
+  render() {
+    const { count } = this.props;
+    
+    const elephantsJsx = [];
+    for (let i = 0; i < count; i++) {
+      elephantsJsx.push(<span>🐘</span>);
+    }
+
+    return (
+      <div className="elephants">
+        {elephantsJsx}
+      </div>
+    );
+  }
+}
+
+Elephant.propTypes = {
+  count: PropTypes.number.isRequired
+}
